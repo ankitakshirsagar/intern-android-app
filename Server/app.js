3c6f5b270110dc143ba4f0cc9a8610cb2f160461
unknown <amk.ankita28@gmail.com>
2020-09-27 00:24:02 +0530

    first commit

---
+++ Server/app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+
+var app = express.createServer();
+
+var fs = require('fs');
+var im = require('imagemagick');
+
+var Db = require('mongodb').Db;
+var dbServer = require('mongodb').Server;
+var dbConnection = require('mongodb').Connection;
+
+var db = new Db('photos', new dbServer('localhost', dbConnection.DEFAULT_PORT, {}));
+db.open(function(err, db){});
+
+app.use(express.bodyParser());
+
+app.get('/', function(req, res){
+    res.send(
+        '<form action="/upload" method="post" enctype="multipart/form-data">'+
+        '<input type="file" name="source">'+
+        '<input type="submit" value="Upload">'+
+        '</form>'
+    );
+});
+
+app.post('/upload', function(req, res){
+	console.log("Received file:\n" + JSON.stringify(req.files));
+	
+	var photoDir = __dirname+"/photos/";
+	var thumbnailsDir = __dirname+"/photos/thumbnails/";
+	var photoName = req.files.source.name;
+	
+	fs.rename(
+		req.files.source.path,
+		photoDir+photoName,
+		function(err){
+			if(err != null){
+				console.log(err)
+				res.send({error:"Server Writting No Good"});
+			} else {
+				im.resize(
+					{
+						srcData:fs.readFileSync(photoDir+photoName, 'binary'),
+						width:256
+					}, 
+					function(err, stdout, stderr){
+						if(err != null){
+							console.log('stdout : '+stdout)
+							
+							res.send({error:"Resizeing No Good"});
+						} else {
+							//console.log('ELSE stdout : '+stdout)
+							fs.writeFileSync(thumbnailsDir+"thumb_"+photoName, stdout, 'binary');
+							res.setHeader('Content-Type', 'application/json');
+							res.statusCode = 202;
+							res.statusMessage = "Image uploaded to the server";
+    						res.send({"store":"XYZ","itemList":[{"item":"","cost":0,"no":0},{"item":"","cost":0,"no":0},{"item":"","cost":0,"no":0}]});
+						}
+					}
+				);
+			}
+		}
+	);
+});
+
+app.get('/info', function(req, res){
+	console.log(__dirname);
+	res.send("ok");
+});
+
+app.listen(8000);
+console.log('connected to localhost....')
